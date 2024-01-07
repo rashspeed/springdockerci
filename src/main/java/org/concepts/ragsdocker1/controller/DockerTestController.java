376c50846cb3ed6a9a1f57ee9cbf1adfb92e5e92
Ragesh <rageshmenon@yahoo.com>
2024-01-07 16:24:39 +0000

    changes to test dockerised ci build

--- src/main/java/org/concepts/ragsdocker1/controller/DockerTestController.java
+++ src/main/java/org/concepts/ragsdocker1/controller/DockerTestController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerTestController {
     @GetMapping
     public String hello() {
-        return "Hello World! Changed";
+        return "Hello World! Docker Changes";
     }
 }
